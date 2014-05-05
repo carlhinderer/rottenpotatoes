a4f53b5f095763111ba64af0eb338e8dc635a9fc
Carl Hinderer <carl.hinderer4@gmail.com>
2014-05-04 23:59:28 -0400

    Adding model, controller, and views for movies.

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,41 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find params[:id]
+    @movie.destroy
+    flash[:notice] = "Movie #{@movie.title} was successfully destroyed."
+    redirect_to movies_path
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+end
