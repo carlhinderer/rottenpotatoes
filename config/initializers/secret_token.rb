c92ed07dc3ee059aa7c46bb66b3dd509cf117d1c
Carl Hinderer <carl.hinderer4@gmail.com>
2014-05-04 23:35:26 -0400

    Initial commit.  Gemfile updated with Haml and debugger.  Resource and migration added for movies.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rottenpotatoes::Application.config.secret_key_base = 'd1016c6a1d2f671ccb063ad2f57dbcf791c5600b341c53ca4119a16c270a057f1e4aa4c43b58c4beddf4217443ca19b1282d42a1243cc295a6b9903787b84e65'
