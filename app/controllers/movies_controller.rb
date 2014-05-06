0ef96ff35fe5ce2546ebb065f06fe2ab99befaef
Carl Hinderer <carl.hinderer4@gmail.com>
2014-05-05 21:05:13 -0400

    Created action for retrieving movies from TMDb.  Refactored cucumber tests for success and failure of retrieval from TMDb.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -34,6 +34,12 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  def search_tmdb
+    # automatically failing at this point
+    flash[:warning] = "'#{params[:search_terms]}' was not found in TMDb."
+    redirect_to movies_path
+  end
+
   private
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rottenpotatoes::Application.routes.draw do
   resources :movies
   root :to => redirect('/movies')
+  
+  # Route that posts 'Search TMDb' form
+  post '/movies/search_tmdb'
 end
